187dab87472a72efb5202b9d4f743aec4f58451c
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-15 05:03:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,14 +1,13 @@
 class Author
   
-  attr_accessor :name 
+  attr_accessor :name, :posts 
+  
+  @@post_count = 0 
   
   def initialize(name)
     @name = name 
+    @posts = []
   end
-  
-  def posts
-    @@posts = []
-  end 
   
   def add_post(post) 
     post.author = self 
