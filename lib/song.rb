1eea90c0b72fbb3c30e32a41bd762dbad63d8830
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-15 04:34:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   
   attr_accessor :artist, :name  
   
-  def intitialize(name)
+  def initialize(name)
     @name = name
     @@all = [] 
     @@all << @name 
@@ -12,10 +12,6 @@ class Song
   def Song.all
     @@all 
   end
-  
-  def name 
-    @name
-  end 
   
   
   
