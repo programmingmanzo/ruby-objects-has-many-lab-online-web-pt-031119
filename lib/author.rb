968fd895601ebf513287436f96094bb284b3030a
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-15 05:05:55 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,15 +10,20 @@ class Author
   end
   
   def add_post(post) 
-    post.author = self 
+    @posts << post 
+    post.author = self
+    @@post_count += 1 
   end 
   
   def add_post_by_title(title)
-    title.author = self 
+    post = Post.new(title)
+    @posts << post 
+    post.author = self
+    @@post_count += 1 
   end 
   
   def self.post_count 
-    @@posts 
+    @@posts_count  
   end 
   
   
